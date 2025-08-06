c19cb87c59cb66482b18d6a893b0aeb22067f023
CuB1z <cub1zdev@gmail.com>
2025-08-06 10:41:13 +0200

    init: Init v2 portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import i18n from "@astrolicious/i18n";
+
+export default defineConfig({
+    integrations: [
+        i18n({
+            defaultLocale: "en",
+            locales: ["en", "es"],
+            strategy: "prefixWithoutDefault",
+        }),
+    ],
+});
